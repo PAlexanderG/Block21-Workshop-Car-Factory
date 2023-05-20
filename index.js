8d5e908b4beba5ab94f31423bf17a5b78e99e9cc
Pedro Alexander Garcia A <pedroalexandergarciaa@pedros-imac.myfiosgateway.com>
2023-05-19 20:28:51 -0400

    working progress

---
+++ index.js
@@ -0,0 +1,8 @@
+// # OVERVIEW
+
+// The automotive industry comprises a wide range of companies
+// and organizations involved in the design, development, manufacturing,
+// marketing, and selling of motor vehicles.
+
+// Creating a virtual assembly line to create some different types of vehicles
+// with as many as the same parts as you possibly can.
